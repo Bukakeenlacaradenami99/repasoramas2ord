2f98b6ad0510626675e56deed8ee25e05ed9cd16
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2024-02-20 16:05:17 +0100

    A

---
+++ src/Cristian.java
@@ -0,0 +1,21 @@
+public class Cristian {
+
+
+
+
+    public void damesuma(int num, int num2){
+
+        int resultado=num+num2;
+        System.out.println(resultado);
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[]args){
+
+
+        Cristian cr=new Cristian();
+        cr.damesuma(3, 4);
+
+
+    }
+
+
+}
